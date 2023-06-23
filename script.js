235c0f2b48fdd47f2f0c8df3d676ea9c90ea549e
apaolaoliveira <paolaoliveira.dev@gmail.com>
2023-06-23 16:12:50 -0300

    add images

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
+const sliders = document.querySelectorAll(".slider")
 
-let id = 0;
+sliders.forEach((slider) => {
+  const slides = slider.querySelector(".slides")
+  const prevBtn = slider.querySelector(".prev")
+  const nextBtn = slider.querySelector(".next")
+  const slideWidth = 590
 
-function carrossel(){
-  id++;
+  let slideIndex = 0
+  let intervalId
 
-  if(id > img.length - 1)
-    id = 0;
+  prevBtn.addEventListener("click", prevSlide)
+  nextBtn.addEventListener("click", nextSlide)
 
-  imgs.style.transform = `translateX(${-id * 500}px)`;
-}
+  startSlider()
 
-setInterval(carrossel, 1800);
+  function startSlider() {
+    intervalId = setInterval(nextSlide, 4000)
+  }
+
+  function prevSlide() {
+    clearInterval(intervalId)
+    if (slideIndex === 0) {
+      slideIndex = slides.childElementCount - 1
+    } else {
+      slideIndex--
+    }
+    updateSlidePosition()
+  }
+
+  function nextSlide() {
+    clearInterval(intervalId) 
+    if (slideIndex === slides.childElementCount - 1) {
+      slideIndex = 0
+    } else {
+      slideIndex++
+    }
+    updateSlidePosition()
+  }
+
+  function updateSlidePosition() {
+    const translateXValue = slideWidth * -slideIndex 
+    slides.style.transform = `translateX(${translateXValue}px)`
+
+    startSlider() 
+  }
+})
